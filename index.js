208ebf6a933d79760fdb5ac7707cb61d4b9783c8
Victor FCh <v.faria@codebay-innovation.com>
2022-02-04 14:38:56 +0000

    use mongo to resolve api calls

--- index.js
+++ index.js
@@ -1,81 +1,76 @@
+require('dotenv').config()
 require('./mongo')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 
-//const Note = require('./models/Note')
+const Note = require('./models/Note')
 
 app.use(cors())
 app.use(express.json())
 
 
-let notes = [
-	{
-		id: 1,
-		content: 'Seguir a midudev',
-		date: '2019-05-30T17:30:31.098Z',
-		important: true
-	},
-	{
-		id: 2,
-		content: 'Ir al gimnasio',
-		date: '2020-07-02T14:21:18.098Z',
-		important: false
-	},
-	{
-		id: 3,
-		content: 'Hacer cursos de fullstack',
-		date: '2021-12-09T20:30:31.098Z',
-		important: true
-
-	}
-]
-
-const STATUS_NOT_FOUND = 404
-const STATUS_MISSING_DATA = 400
-const STATUS_NOT_CONTENT = 204
+const NOT_CONTENT = 204
+const BAD_REQUEST = 400
+const NOT_FOUND = 404
+const INTERNAL_SERVER_ERROR = 500
 
 app.get('/', (request, response) => {
 	response.send('<h1>Hello world</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-	response.json(notes)
+	Note.find({}).then((notes) => {
+		response.json(notes)
+	})
 })
 
-app.get('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const note = notes.find(note => note.id = id)
-	if (!note) return response.status(STATUS_NOT_FOUND).end()
-	response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+	const id = request.params.id
+
+	Note.findById(id)
+		.then(note => {
+			if (!note)
+				return response.status(NOT_FOUND).end()
+			response.json(note)
+		})
+		.catch(err => next(err))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	notes = notes.filter(note => note.id != id)
-	response.status(STATUS_NOT_CONTENT).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+	const id = request.params.id
+	Note.findByIdAndRemove(id).then(() => {
+		response.status(NOT_CONTENT).end()
+	}).catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
 	const body = request.body
 
 	if (!body || !body.content) 
-		return response.status(STATUS_MISSING_DATA).json({
+		return response.status(BAD_REQUEST).json({
 			error: 'content is missing'
 		})
 
-	const ids = notes.map(note => note.id)
-	const nextId = Math.max(...ids)
-	const newNote = {
-		id: nextId + 1,
+	const newNote = new Note({
 		content: body.content,
-		date: new Date().toISOString(),
-		important: typeof body.important != 'undefined' 
-			? body.important
-			: false
+		date: new Date(),
+		important: body.important || false
+	})
+	newNote.save().then(savedNote => {
+		response.json(savedNote)
+	})
+})
+
+app.use((error, request, response, next) => {
+	console.log(error.name)
+	if (error.name === 'CastError') {
+		response.status(BAD_REQUEST).send({
+			error: 'id used is malformed'
+		})
+	} else {
+		response.status(INTERNAL_SERVER_ERROR).end()
 	}
-	notes = [...notes, newNote]
-	response.json(newNote)
 })
 
 const PORT = process.env.PORT
